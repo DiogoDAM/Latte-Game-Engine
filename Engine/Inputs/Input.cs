e3ce01ece9e0f6a47ade539d94dc24215e707eb5
DiogoDAM <diogo.andradem20@protonmail.com>
2026-03-17 20:37:20 -0300

    Create and Add MouseManager to Input

--- Engine/Inputs/Input.cs
+++ Engine/Inputs/Input.cs
@@ -3,14 +3,17 @@ namespace Latte;
 public sealed class Input
 {
 	public KeyboardManager Keyboard { get; private set; }
+	public MouseManager Mouse { get; private set; }
 
 	public Input()
 	{
 		Keyboard = new();
+		Mouse = new();
 	}
 
 	public void Update(float dt)
 	{
 		Keyboard.Update();
+		Mouse.Update();
 	}
 }
---
+++ Engine/Inputs/MouseMananger.cs
@@ -0,0 +1,73 @@
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Latte;
+
+public sealed class MouseManager 
+{
+	private MouseState _prev;
+	private MouseState _curr;
+
+	public Point CursorPosition => _curr.Position;
+
+	public int ScroolWheelValue => _curr.ScrollWheelValue;
+
+	public MouseManager()
+	{
+		_prev = Mouse.GetState();
+		_curr = Mouse.GetState();
+	}
+
+	public void Update()
+	{
+		_prev = _curr;
+		_curr = Mouse.GetState();
+	}
+
+	public bool IsButtonDown(byte button)
+	{
+		switch (button)
+		{
+			case 0: return _curr.LeftButton == ButtonState.Pressed;
+			case 1: return _curr.RightButton == ButtonState.Pressed;
+			case 2: return _curr.MiddleButton == ButtonState.Pressed;
+			default: throw new NotImplementedException("Button value not implemented, only values (0, 1, 2)");
+		}
+	}
+
+	public bool IsButtonUp(byte button)
+	{
+		switch (button)
+		{
+			case 0: return _curr.LeftButton == ButtonState.Released;
+			case 1: return _curr.RightButton == ButtonState.Released;
+			case 2: return _curr.MiddleButton == ButtonState.Released;
+			default: throw new NotImplementedException("Button value not implemented, only values (0, 1, 2)");
+		}
+	}
+
+	public bool IsButtonPressed(byte button)
+	{
+		switch (button)
+		{
+			case 0: return _curr.LeftButton == ButtonState.Pressed && _prev.LeftButton == ButtonState.Released;
+			case 1: return _curr.RightButton == ButtonState.Pressed && _prev.RightButton == ButtonState.Released;
+			case 2: return _curr.MiddleButton == ButtonState.Pressed && _prev.MiddleButton == ButtonState.Released;
+			default: throw new NotImplementedException("Button value not implemented, only values (0, 1, 2)");
+		}
+	}
+
+	public bool IsButtonReleased(byte button)
+	{
+		switch (button)
+		{
+			case 0: return _curr.LeftButton == ButtonState.Released && _prev.LeftButton == ButtonState.Pressed;
+			case 1: return _curr.RightButton == ButtonState.Released && _prev.RightButton == ButtonState.Pressed;
+			case 2: return _curr.MiddleButton == ButtonState.Released && _prev.MiddleButton == ButtonState.Pressed;
+			default: throw new NotImplementedException("Button value not implemented, only values (0, 1, 2)");
+		}
+	}
+
+}
