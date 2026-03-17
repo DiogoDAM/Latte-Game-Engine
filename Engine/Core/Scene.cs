c42714c39d5e4efe641435352df486587d4f9675
DiogoDAM <diogo.andradem20@protonmail.com>
2026-03-17 14:55:45 -0300

    Add Scene base class

---
+++ Engine/Core/Scene.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Latte;
+
+public class Scene : Behaviour
+{
+	public Scene()
+	{
+	}
+
+	public virtual void Activate()
+	{
+	}
+
+	public virtual void Disable()
+	{
+	}
+}
