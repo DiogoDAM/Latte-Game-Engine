e98b138cac8eaa7fa48c1ef650bf3074e72ebf84
DiogoDAM <diogo.andradem20@protonmail.com>
2026-03-18 20:45:00 -0300

    Add LoadFromXmlFile() to Input class

--- Engine/Inputs/Input.cs
+++ Engine/Inputs/Input.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System;
 
 using Microsoft.Xna.Framework.Input;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Latte;
 
@@ -71,7 +74,7 @@ public sealed class Input
 		if(!_singleActions.ContainsKey(name))
 			throw new KeyNotFoundException($"The action name: {name} not found");
 
-		if(_singleActions[name].Count == 0)
+		if(_singleActions[name].Count == 1)
 			return _singleActions[name][0].IsInAction();
 		else
 		{
@@ -83,5 +86,86 @@ public sealed class Input
 		}
 
 		return false;
+	}
+
+	public void LoadFromXmlFile(string filePath)
+	{
+		string fullPath = Path.Combine(Engine.Content.RootDirectory, filePath);
+
+		using(XmlReader reader = XmlReader.Create(fullPath))
+		{
+			XDocument doc = XDocument.Load(reader);
+			XElement root = doc.Root;
+
+			var inputActions = root?.Elements("InputAction");
+
+			if(inputActions == null)
+				return;
+
+			foreach(var inputAction in inputActions)
+			{
+				string name = inputAction.Attribute("name")?.Value;
+				CreateInputAction(name);
+
+				var keysActions = inputAction?.Elements("Key");
+
+				if(keysActions != null)
+				{
+					foreach(var keyAction in keysActions)
+					{
+						Predicate<Keys> keyPredicate;
+						string type = keyAction?.Attribute("type")?.Value;
+
+						switch(type)
+						{
+							case "down": keyPredicate = Keyboard.IsKeyDown; break;
+							case "up": keyPredicate = Keyboard.IsKeyUp; break;
+							case "pressed": keyPredicate = Keyboard.IsKeyPressed; break;
+							case "released": keyPredicate = Keyboard.IsKeyReleased; break;
+							default: throw new NotImplementedException("KeyboardInputAction type not implemented: " + type);
+						}
+
+						if(Enum.TryParse(keyAction?.Value, true, out Keys key))
+							AddKeyboardInputAction(name, keyPredicate, key);
+						else
+							throw new Exception($"KeyboardInputAction of the name: {name} error to parse key value");
+					}
+				}
+
+				var mouseButtonActions = inputAction?.Elements("MouseButton");
+
+				if(mouseButtonActions != null)
+				{
+					foreach(var mouseButtonAction in mouseButtonActions)
+					{
+						Predicate<byte> mouseButtonPredicate;
+						string type = mouseButtonAction?.Attribute("type")?.Value;
+
+						switch(type)
+						{
+							case "down": mouseButtonPredicate = Mouse.IsButtonDown; break;
+							case "up": mouseButtonPredicate = Mouse.IsButtonUp; break;
+							case "pressed": mouseButtonPredicate = Mouse.IsButtonPressed; break;
+							case "released": mouseButtonPredicate = Mouse.IsButtonReleased; break;
+							default: throw new NotImplementedException("MouseButtonInputAction type not implemented: " + type);
+						}
+
+						byte button;
+
+						string buttonType = mouseButtonAction?.Value;
+
+						switch(buttonType)
+						{
+							case "LMB": button = 0; break;
+							case "RMB": button = 1; break;
+							case "MMB": button = 2; break;
+							default: throw new NotImplementedException("MouseButtonInputAction button type not implemented: " + buttonType);
+						}
+
+						AddMouseButtonInputAction(name, mouseButtonPredicate, button);
+					}
+				}
+			}
+		}
 	}
 }
