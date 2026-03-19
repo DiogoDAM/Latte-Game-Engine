d3356045ee6840af1815e8028bb48301088a4095
DiogoDAM <diogo.andradem20@protonmail.com>
2026-03-19 15:59:51 -0300

    Create CameraComponent.cs

--- obj/Debug/net9.0/Latte-Engine.AssemblyInfo.cs
+++ obj/Debug/net9.0/Latte-Engine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Latte-Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e3f2ed880cb5ef81f932f70e04c0c341fdd2af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e98b138cac8eaa7fa48c1ef650bf3074e72ebf84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Latte-Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Latte-Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
