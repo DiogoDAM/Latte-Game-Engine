7da177ad9ee1f62758a2fe238f2ce082f4bf88af
DiogoDAM <diogo.andradem20@protonmail.com>
2026-03-19 21:15:16 -0300

    Create ColliderCOmponent

---
+++ Engine/Collisions/ColliderComponent.cs
@@ -0,0 +1,56 @@
+using System;
+
+using Microsoft.Xna.Framework;
+
+namespace Latte;
+
+public sealed class ColliderComponent : Component
+{
+	public Shape Shape { get; private set; }
+
+	public Vector2 Offset;
+
+	public bool CanCollide = true;
+
+	public ColliderComponent(Shape shape)
+	{
+		Shape = shape;
+	}
+
+	public void SetShape(Shape shape) => Shape = shape;
+
+    public override void Update(float dt)
+    {
+		Shape.X = (int)(Entity.Transform.Position.X + Offset.X);
+		Shape.Y = (int)(Entity.Transform.Position.Y + Offset.Y);
+    }
+
+	public bool Collides(ColliderComponent col)
+	{
+		if(CanCollide && col.CanCollide)
+			return Shape.Intersects(col.Shape);
+
+		return false;
+	}
+
+	public bool Collides(Entity e)
+	{
+		if(e.TryGetComponent<ColliderComponent>(out ColliderComponent col))
+		{
+			if(CanCollide && col.CanCollide)
+			{
+				return Shape.Intersects(col.Shape);
+			}
+		}
+
+		return false;
+	}
+
+	public bool Contains(Vector2 point)
+	{
+		if(CanCollide)
+			return Shape.Contains(point);
+
+		return false;
+	}
+}
